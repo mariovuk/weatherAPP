5b11e0fa0d2cc5581496eebe3eebe88adaabae4a
mariovuk <mariovuk@yahoo.com>
2017-07-06 12:32:28 +0200

    .

--- app/src/main/java/com/weatherapp/MainActivity.java
+++ app/src/main/java/com/weatherapp/MainActivity.java
@@ -1,16 +1,8 @@
 package com.weatherapp;
 
-import android.content.Context;
-import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -20,7 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
@@ -35,9 +26,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity
@@ -45,7 +33,7 @@ public class MainActivity extends AppCompatActivity
 
 
     TextView textView1;
-    Button button1;
+
 
 
     private static final String APP_ID = "b5362c203397ce801d354458c2262485";
@@ -57,26 +45,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(com.weatherapp.R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(com.weatherapp.R.id.toolbar);
         setSupportActionBar(toolbar);
-
         textView1=(TextView)findViewById(R.id.textView1);// text to show addresses
-        button1=(Button)findViewById(R.id.button1); // button
-
-        // click the below to get the current location and address.
-        button1.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-
-                turnGPSOn(); // method to turn on the GPS if its in off state.
-                getMyCurrentLocation();
-
-            }
-        });
-
-
-
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(com.weatherapp.R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -89,13 +58,29 @@ public class MainActivity extends AppCompatActivity
 
         //Code for temperature
 
-        double lat = 59.3275, lon = 18.0675;
-        String units = "metric";
-        String url = String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=%s&appid=%s",
-                lat, lon, units, APP_ID);
+        double lat1 = 44.7866, lon1 = 20.4489;
+        double lat2 = 41.3851, lon2 = 2.1734;
+        double lat3 = 59.3275, lon3 = 18.0675;
 
-        TextView textView = (TextView) findViewById(R.id.temperature);
-        new GetWeatherTask(textView).execute(url);
+        String units = "metric";
+
+        String url1 = String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=%s&appid=%s",
+                lat1, lon1, units, APP_ID);
+
+        TextView textView = (TextView) findViewById(R.id.temperature1);
+        new GetWeatherTask(textView).execute(url1);
+
+        String url2 = String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=%s&appid=%s",
+                lat2, lon2, units, APP_ID);
+
+        TextView textView1 = (TextView) findViewById(R.id.temperature2);
+        new GetWeatherTask(textView1).execute(url2);
+
+        String url3 = String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=%s&appid=%s",
+                lat3, lon3, units, APP_ID);
+
+        TextView textView2 = (TextView) findViewById(R.id.temperature3);
+        new GetWeatherTask(textView2).execute(url3);
     }
 
     @Override
@@ -187,215 +172,5 @@ public class MainActivity extends AppCompatActivity
             textView.setText("Current temperature: " + temp + "c");
         }
     }
-
-
-
-    /** Method to turn on GPS **/
-    public void turnGPSOn(){
-        try
-        {
-
-            String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-
-
-            if(!provider.contains("gps")){ //if gps is disabled
-                final Intent poke = new Intent();
-                poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
-                poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
-                poke.setData(Uri.parse("3"));
-                sendBroadcast(poke);
-            }
-        }
-        catch (Exception e) {
-
-        }
-    }
-    // Method to turn off the GPS
-    public void turnGPSOff(){
-        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-
-        if(provider.contains("gps")){ //if gps is enabled
-            final Intent poke = new Intent();
-            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
-            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
-            poke.setData(Uri.parse("3"));
-            sendBroadcast(poke);
-        }
-    }
-
-    // turning off the GPS if its in on state. to avoid the battery drain.
-    @Override
-    protected void onDestroy() {
-        // TODO Auto-generated method stub
-        super.onDestroy();
-        turnGPSOff();
-    }
-
-    /**
-     * Check the type of GPS Provider available at that instance and
-     * collect the location informations
-     *
-     * @Output Latitude and Longitude
-     */
-    void getMyCurrentLocation() {
-
-
-        LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locListener = new MyLocationListener();
-
-
-        try{gps_enabled=locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);}catch(Exception ex){}
-        try{network_enabled=locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);}catch(Exception ex){}
-
-        //don't start listeners if no provider is enabled
-        //if(!gps_enabled && !network_enabled)
-        //return false;
-
-        if(gps_enabled){
-            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
-
-        }
-
-
-        if(gps_enabled){
-            location=locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-
-        }
-
-
-        if(network_enabled && location==null){
-            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locListener);
-
-        }
-
-
-        if(network_enabled && location==null)    {
-            location=locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-        }
-
-        if (location != null) {
-
-            MyLat = location.getLatitude();
-            MyLong = location.getLongitude();
-
-
-        } else {
-            Location loc= getLastKnownLocation(this);
-            if (loc != null) {
-
-                MyLat = loc.getLatitude();
-                MyLong = loc.getLongitude();
-
-
-            }
-        }
-        locManager.removeUpdates(locListener); // removes the periodic updates from location listener to //avoid battery drainage. If you want to get location at the periodic intervals call this method using //pending intent.
-
-        try
-        {
-// Getting address from found locations.
-            Geocoder geocoder;
-
-            List<Address> addresses;
-            geocoder = new Geocoder(this, Locale.getDefault());
-            addresses = geocoder.getFromLocation(MyLat, MyLong, 1);
-
-            StateName= addresses.get(0).getAdminArea();
-            CityName = addresses.get(0).getLocality();
-            CountryName = addresses.get(0).getCountryName();
-            // you can get more details other than this . like country code, state code, etc.
-
-
-            System.out.println(" StateName " + StateName);
-            System.out.println(" CityName " + CityName);
-            System.out.println(" CountryName " + CountryName);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-
-        textView1.setText(" StateName " + StateName +" CityName " + CityName +" CountryName " + CountryName);
-    }
-
-    // Location listener class. to get location.
-    public class MyLocationListener implements LocationListener {
-        public void onLocationChanged(Location location) {
-            if (location != null) {
-            }
-        }
-
-        public void onProviderDisabled(String provider) {
-            // TODO Auto-generated method stub
-        }
-
-        public void onProviderEnabled(String provider) {
-            // TODO Auto-generated method stub
-        }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            // TODO Auto-generated method stub
-        }
-    }
-
-    private boolean gps_enabled=false;
-    private boolean network_enabled=false;
-    Location location;
-
-    Double MyLat, MyLong;
-    String CityName="";
-    String StateName="";
-    String CountryName="";
-
-// below method to get the last remembered location. because we don't get locations all the times .At some instances we are unable to get the location from GPS. so at that moment it will show us the last stored location.
-
-    public static Location getLastKnownLocation(Context context)
-    {
-        Location location = null;
-        LocationManager locationmanager = (LocationManager)context.getSystemService("location");
-        List list = locationmanager.getAllProviders();
-        boolean i = false;
-        Iterator iterator = list.iterator();
-        do
-        {
-            //System.out.println("---------------------------------------------------------------------");
-            if(!iterator.hasNext())
-                break;
-            String s = (String)iterator.next();
-            //if(i != 0 && !locationmanager.isProviderEnabled(s))
-            if(i != false && !locationmanager.isProviderEnabled(s))
-                continue;
-            // System.out.println("provider ===> "+s);
-            Location location1 = locationmanager.getLastKnownLocation(s);
-            if(location1 == null)
-                continue;
-            if(location != null)
-            {
-                //System.out.println("location ===> "+location);
-                //System.out.println("location1 ===> "+location);
-                float f = location.getAccuracy();
-                float f1 = location1.getAccuracy();
-                if(f >= f1)
-                {
-                    long l = location1.getTime();
-                    long l1 = location.getTime();
-                    if(l - l1 <= 600000L)
-                        continue;
-                }
-            }
-            location = location1;
-            // System.out.println("location  out ===> "+location);
-            //System.out.println("location1 out===> "+location);
-            i = locationmanager.isProviderEnabled(s);
-            // System.out.println("---------------------------------------------------------------------");
-        } while(true);
-        return location;
-    }
-
-
-
 }
 
