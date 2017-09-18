0b5de77e0fb84edd45c9147a531349c1b84da493
mariovuk <mariovuk@yahoo.com>
2017-09-18 07:31:59 +0200

    Revert "."
    This reverts commit 77a2c519c7b56885683166f7697152c277aabdba.
    
    # Conflicts:
    #	app/src/main/java/com/weatherapp/MainActivity.java

--- app/src/main/java/com/weatherapp/MainActivity.java
+++ app/src/main/java/com/weatherapp/MainActivity.java
@@ -3,8 +3,6 @@ package com.weatherapp;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,7 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
